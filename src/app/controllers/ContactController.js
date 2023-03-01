927e09e8ded9f91f46aea3a14588289cdcda6a88
guiibraun <guiabraun@hotmail.com>
2023-02-28 22:08:08 -0300

    Update routes, package.json, .eslintrc.js, and ContactcsRepository.js files

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -4,24 +4,86 @@ class ContactController {
   async index(request, response) {
     // Listar todos os registros
     const contacts = await ContactsRepository.findAll();
-
     response.json(contacts);
+    response.send(request.appID);
   }
 
-  show() {
+  async show(request, response) {
     // Obter um registro
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      // 404: Not found
+      return response.status(400).json({ error: 'Contact not found' });
+    }
+
+    response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
     // Criar novo registro
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+    response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
     // Editar um registro
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExist = await ContactsRepository.findById(id);
+
+    if (!contactExist) {
+      return response.status(400).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+    console.log(contactByEmail);
+
+    if (contactByEmail && contactByEmail.id === id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
-  delete() {
+  async delete(request, response) {
     // Deletar um registro
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+    await ContactsRepository.delete(id);
+    response.sendStatus(204);
   }
 }
 
--- src/app/repositories/ContactcsRepository.js
+++ src/app/repositories/ContactcsRepository.js
@@ -1,16 +1,81 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [{
-  id: uuid(),
-  name: 'Guilherme',
-  email: 'guiabraun@hotmail.com',
-  category_id: uuid(),
-}];
+let contacts = [
+  {
+    id: v4(),
+    name: 'Guilherme',
+    email: 'guiabraun@hotmail.com',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Mariene',
+    email: 'guiabraun@hotmail.com',
+    category_id: v4(),
+  },
+];
 
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.email === email));
+    });
+  }
+
+  create({
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts.push(newContact);
+      resolve(newContact);
+    });
+  }
+
+  update(id, {
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts = contacts.map((contact) => {
+        if (contact.id === id) {
+          return updatedContact;
+        }
+
+        return contact;
+      });
+      resolve(updatedContact);
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
